f6fdaf79817e5b5e9bad3a0821adc371aa87a3b5
faticoco1 <167507596+faticoco1@users.noreply.github.com>
2024-04-23 16:57:23 +0500

    Update iridium.test.js

--- src/iridium.test.js
+++ src/iridium.test.js
@@ -1,5 +1,8 @@
 const { getTable } = require('./iridium');
 
+// Mock the 'request' module completely
+jest.mock('request', () => jest.fn());
+
 describe('Iridium Module', () => {
   test('getTable function retrieves table data from URL', () => {
     // Mock configuration object for getTable function
@@ -11,16 +14,10 @@ describe('Iridium Module', () => {
       pages: 1 // Number of pages to fetch
     };
 
-    // Mock HTTP request function
-    const mockedRequest = jest.fn();
-
-    // Override request module with mocked function
-    jest.mock('request', () => jest.fn().mockImplementation(mockedRequest));
-
     // Call the getTable function
     getTable(config);
 
     // Expect the mocked request function to have been called
-    expect(mockedRequest).toHaveBeenCalled();
+    expect(require('request')).toHaveBeenCalled();
   });
 });
